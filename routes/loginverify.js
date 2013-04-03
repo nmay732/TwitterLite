d59f7103eb4059e9232a1779b8e299d5a8694ee6
nmay732 <nmay@student.umass.edu>
2013-04-03 14:53:02 -0400

    fixed syntax so server would run

--- routes/loginverify.js
+++ routes/loginverify.js
@@ -4,27 +4,24 @@ var Users = require('../lib/user');
 
 
 exports.loginverify = function(req, res){
-  var users=[
+    var users=[
       {username:'bob',password:'b'},
       {username:'alice',password:'a'},
       {username:'jack',password:'j'},
       {username:'mick',password:'m'} 
       ]
 
- var user=req.query.username;
-        var password=req.query.password;
-        for (var i=0;i<users.length;i++)
-          if(users[i].username===user)
-            if(users[i].password===password)
-            {
+    var user=req.query.username;
+    var password=req.query.password;
+    for (var i=0;i<users.length;i++){
+        if(users[i].username===user)
+            if(users[i].password===password){
               loginUser=user;
               console.log("Login Success");
               res.redirect('/dashboard');
             }
              // loginCount++;
               res.redirect('/login');
-          
-
-}
+    }
 
 }
