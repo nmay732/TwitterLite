338d039dc272db90ee0a240d6644cdd87547f41c
nmay732 <nmay@student.umass.edu>
2013-04-04 00:46:50 -0400

    Better fake data, updated in line js for dash

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,31 +9,31 @@ var users=[
     // we can call addfollowers function to add more followers
     // add by Billy
 var followers=[
-    {username:'bob',follower:'alice'},
-    {username:'bob',follower:'jack'},
     {username:'bob',follower:'mick'},
+    {username:'bob',follower:'alice'},
+    {username:'alice',follower:'mick'},
+    {username:'alice',follower:'bob'},
+    {username:'jack',follower:'mick'},
+    {username:'jack',follower:'bob'},
+    {username:'mick',follower:'jack'}
     ]
 
 // we may add more tweet by enter text in broser 
 // we need this function
 var Tweet=[
-    {username:'bob',  text:"How are you ?"},
-    {username:'alice',text:"What are you doing ?"},
-    {username:'jack', text:"Are you going to party tonight?"},
-    {username:'mick', text:"Are you going tonight?"}
+    {username:'bob',  tag: "alice", text:"@alice How was class today?"},
+    {username:'bob',  tag: null, text:"Too sick to go to class today :("},
+    {username:'alice', tag: null, text:"I am so happy!!!"},
+    {username:'alice', tag: null, text:"I love my sorority sisters!!!"},
+    {username:'jack', tag: null, text:"Dinner at bubs"},
+    {username:'jack', tag: null, text:"Anyone want to go to High Horse tonight?"},
+    {username:'mick', tag: null, text:"The forest speaks to me sometimes.."},
+    {username:'mick', tag: null, text:"I am always listening"}
     ]
-
-  // we can add information to user profile
-var info=[
-    {username:'bob',password:'b',hometown:'China',birthday:'12/12/1987'},
-    {username:'alice',password:'a',hometown:'U S',birthday:'10/10/1991'},
-    {username:'jack',password:'j',hometown:'China',birthday:'12/12/1978'},
-    {username:'mick',password:'m',hometown:'England',birthday:'01/01/1990'}
-    ]
-
 
 var loginUser=null; //TODO: Get users from cookies
 
+//Until we get the flash function working we won't know what user is logged in. change it manually here
 res.render('dashboard.ejs', {user: 'bob', followers: followers, tweets: Tweet});
 
 }
